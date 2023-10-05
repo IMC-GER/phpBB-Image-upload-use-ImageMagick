c6b0a0dc32f0b7bbb6593434d23e239a70190be1
Thorsten <88725484+IMC-GER@users.noreply.github.com>
2023-10-05 20:50:27 +0200

    1.3.0-a3
    - replace echo and exit_handler() with the function trigger_error()

--- imcger/imgupload/core/save_rotated_img.php
+++ imcger/imgupload/core/save_rotated_img.php
@@ -14,6 +14,7 @@
  * @ignore
  */
 define('IN_PHPBB', true);
+global $user, $auth, $request, $language, $db, $config;
 $phpbb_root_path = './../../../../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);
@@ -22,7 +23,6 @@ include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
 // Start session management
 $user->session_begin();
 $auth->acl($user->data);
-$user->setup('viewforum');
 
 // No ajax request, redirect to forum index
 if (!$request->is_ajax())
@@ -33,8 +33,8 @@ if (!$request->is_ajax())
 // No user logged in, redirect in js to login page
 if ($user->data['user_id'] == ANONYMOUS)
 {
-	echo 'LOGIN_REQUIRED';
-	exit_handler();
+	// The flag E_USER_WARNING trigger a redirect to the login page
+	trigger_error('LOGIN_REQUIRED', E_USER_WARNING);
 }
 
 // Add language file
@@ -44,16 +44,14 @@ $data = $request->variable('data', '');
 
 if (!$data)
 {
-	echo $language->lang('IUL_NO_DATA_SEND');
-	exit_handler();
+	trigger_error($language->lang('IUL_NO_DATA_SEND'), E_USER_NOTICE);
 }
 
 list($img_attach_id, $img_rotate_deg) = explode(';', $data);
 
 if (!$img_attach_id || !$img_rotate_deg)
 {
-	echo $language->lang('IUL_WRONG_PARAM');
-	exit_handler();
+	trigger_error($language->lang('IUL_WRONG_PARAM'), E_USER_NOTICE);
 }
 
 if ($auth->acl_gets('u_attach', 'a_attach', 'f_attach'))
@@ -69,8 +67,7 @@ if ($auth->acl_gets('u_attach', 'a_attach', 'f_attach'))
 
 if (!isset($img_data) || $img_data == false)
 {
-	echo $language->lang('IUL_NO_IMG_IN_DATABASE');
-	exit_handler();
+	trigger_error($language->lang('IUL_NO_IMG_IN_DATABASE'), E_USER_NOTICE);
 }
 
 // Get image file path
@@ -79,22 +76,20 @@ $thumb_file_path = $phpbb_root_path . trim($config['upload_path'], '/') . '/' . 
 
 if (file_exists($image_file_path))
 {
-    $img_data['filesize'] = rotate_image($image_file_path, $img_rotate_deg);
+	$img_data['filesize'] = rotate_image($image_file_path, $img_rotate_deg);
 }
 else
 {
-	echo $language->lang('IUL_IMG_NOT_EXIST');
-	exit_handler();
+	trigger_error($language->lang('IUL_IMG_NOT_EXIST'), E_USER_NOTICE);
 }
 
 if ($img_data['thumbnail'] && file_exists($thumb_file_path))
 {
-    rotate_image($thumb_file_path, $img_rotate_deg);
+	rotate_image($thumb_file_path, $img_rotate_deg);
 }
 else if ($img_data['thumbnail'])
 {
-	echo $language->lang('IUL_THUMB_NOT_EXIST');
-	exit_handler();
+	trigger_error($language->lang('IUL_THUMB_NOT_EXIST'), E_USER_NOTICE);
 }
 
 // Update DataBase
@@ -105,17 +100,16 @@ $db->sql_query($sql);
 // sql_nextid() to be removed in 4.1.0-a1, in future use sql_last_inserted_id() it exsits since 3.3.11-RC1
 $new_attachID = $db->sql_nextid();
 
-if ($new_attachID) {
+if ($new_attachID)
+{
 	$sql = 'DELETE FROM ' . ATTACHMENTS_TABLE . ' WHERE attach_id = ' . (int) $img_attach_id;
 	$db->sql_query($sql);
 
-	echo 'UPDATE-ROW_' . $img_attach_id . '_' . $new_attachID;
-	exit_handler();
+	trigger_error('UPDATE-ROW_' . $img_attach_id . '_' . $new_attachID, E_USER_NOTICE);
 }
 else
 {
-	echo $language->lang('IUL_DATABASE_NOT_UPDATE');
-	exit_handler();
+	trigger_error($language->lang('IUL_DATABASE_NOT_UPDATE'), E_USER_NOTICE);
 }
 
 
